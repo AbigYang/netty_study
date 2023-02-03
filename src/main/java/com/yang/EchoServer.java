fff66258630243eacca34d4570a106c0b3543e9b
ick <yang_yangyue@163.com>
2023-02-03 18:02:39 +0800

    Initial commit

---
+++ src/main/java/com/yang/EchoServer.java
@@ -0,0 +1,57 @@
+package com.yang;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.sctp.nio.NioSctpServerChannel;
+
+import java.net.InetSocketAddress;
+
+public class EchoServer {
+    private final int port;
+
+    public EchoServer(int port) {
+        this.port = port;
+    }
+
+    public static void main(String[] args) throws Exception {
+//        if (args.length != 1) {
+//            System.out.println("Usage: " + EchoServer.class.getSimpleName() + " ");
+//        }
+        int port = 8888;
+        new EchoServer(port).start();
+    }
+
+    public void start() throws Exception {
+        final EchoServerHandler serverHandler = new EchoServerHandler();
+        //创建Event-LoopGroup
+        EventLoopGroup group = new NioEventLoopGroup();
+        try {
+            //创建Server-Bootstrap
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(group)
+                    //指定所使⽤的NIO传输Channel
+                    .channel(NioSctpServerChannel.class)
+                    //使⽤指定的端⼝设置套接字地址
+                    .localAddress(new InetSocketAddress(port))
+                    //添加⼀个EchoServer-Handler到⼦Channel的ChannelPipeline
+                    .childHandler(new ChannelInitializer() {
+                        @Override
+                        protected void initChannel(Channel ch) throws Exception {
+                            ////EchoServerHandler被标注为@Shareable，所以我们可以总是使⽤同样的实例
+                            ch.pipeline().addLast(serverHandler);
+                        }
+                    });
+            //异步地绑定服务器；调⽤sync()⽅法阻塞等待直到绑定完成
+            ChannelFuture f = b.bind().sync();
+            //获取Channel的CloseFuture，并且阻塞当前线程直到它完成
+            f.channel().closeFuture().sync();
+        } finally {
+            //关闭EventLoopGroup，释放所有的资源
+            group.shutdownGracefully().sync();
+        }
+    }
+}
---
+++ src/main/java/com/yang/EchoServerHandler.java
@@ -0,0 +1,33 @@
+package com.yang;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.CharsetUtil;
+
+public class EchoServerHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf in = (ByteBuf) msg;
+        System.out.println("Server received: " + in.toString(CharsetUtil.UTF_8));
+        //将接收到的消息写给发送者，⽽不冲刷出站消息
+        ctx.write("i received" + in.toString(CharsetUtil.UTF_8));
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        //将未决消息冲刷到远程节点，并且关闭该Channel
+        //  未 决 消 息 （ pending message ） 是 指 ⽬ 前 暂 存 于ChannelOutboundBuffer 中 的 消 息 ，
+        //  在 下 ⼀ 次 调 ⽤ flush() 或 者writeAndFlush()⽅法时将会尝试写出到套接字
+        ctx.writeAndFlush(Unpooled.EMPTY_BUFFER).addListener(ChannelFutureListener.CLOSE);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        //关闭该Channel
+        ctx.close();
+    }
+}
